7d1f011523e5c14018a3a9e71506b36b3ebed896
mohadeseh <mohadeseh.alipour27@gmail.com>
2025-01-07 23:34:36 +0100

    feature: configuration of docker and workflow

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,6 @@ import Form from "./Form";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
